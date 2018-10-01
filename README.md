ff3efc4d1b5d886f02db7aa63b6a4a5811ea09bc
Ramesh Nair <ram@hiddentao.com>
2018-10-02 00:01:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# KickBack Mixpanel proxy server
+
+[![Build Status](https://travis-ci.org/noblocknopartymixpanel-proxy.svg?branch=dev)](https://travis-ci.org/noblocknopartymixpanel-proxy)
+
+* Live URL: https://analytics.kickback.events
+
+Because some browsers and ad-blockers prevent the Mixpanel client JS libary from working,
+we use this server to proxy requests from our app to Mixpanel.
+
+## Usage
+
+First, deploy this server at a URL, e.g. `mydomain.com`.
+
+Follow the [standard Mixpanel instructions](https://mixpanel.com/help/reference/javascript) to
+include Mixpanel into your HTML file.
+
+Then, put the following code before the Mixpanel code you just embedded:
+
+```html
+<script type="text/javascript">
+  window.MIXPANEL_CUSTOM_LIB_URL = "http://mydomain.com/client.js";
+</script>
+```
+
+That's it! Now you can use all the normal Mixpanel APIs in your page and
+everything will get sent to Mixpanel despite ad blockers.
+
+_Note: Due to the proxy server, the page visitor's IP address does not get
+sent to Mixpanel, which means country and city resolution doesn't work. I am
+still figuring out how to fix this!_
+
+## Deployment
+
+The `master` branch is auto-deployed by Travis to the live URL.
+
+## License
+
+AGPL v3
---
+++ index.js
@@ -0,0 +1,74 @@
+const got = require('got')
+const Koa = require('koa')
+const KoaRouter = require('koa-router')
+const globalTunnel = require('global-tunnel-ng')
+
+const { PROXY } = process.env
+
+if (PROXY) {
+  const [ host, port ] = PROXY.split(':')
+  console.log(`Proxying all requests through ${host}:${port}`)
+  globalTunnel.initialize({ host, port })
+}
+
+const MIXPANEL_API_URL = 'https://api.mixpanel.com'
+const MIXPANEL_JS_LIB_URL = 'http://cdn4.mxpnl.com/libs/mixpanel-2-latest.min.js'
+
+const CODE_CACHE = new Map()
+
+const init = async () => {
+  console.log(`Downloading JS lib from: ${MIXPANEL_JS_LIB_URL}`)
+
+  const { body: mixpanelJs } = await got(MIXPANEL_JS_LIB_URL)
+
+  const app = new Koa()
+  const router = new KoaRouter()
+
+  router.get('/client.js', async ctx => {
+    const { origin } = ctx
+
+    if (!CODE_CACHE[origin]) {
+      CODE_CACHE[origin] = mixpanelJs.replace(MIXPANEL_API_URL, origin)
+    }
+
+    ctx.body = CODE_CACHE[origin]
+  })
+
+  router.get('/favicon.ico', async ctx => {
+    ctx.body = ''
+  })
+
+  router.get('*', async ctx => {
+    const { origin, href } = ctx
+    const newUrl = `${MIXPANEL_API_URL}${href.substr(origin.length)}`
+
+    const headers = {
+      ...ctx.headers,
+      'X-Forwarded-For': ctx.ip
+    }
+    delete headers.host
+
+    const { body, statusCode, rawHeaders } = await got(newUrl, {
+      headers,
+      ...(PROXY ? { agent: false } : null)
+    })
+
+    ctx.body = body
+    ctx.status = statusCode
+    ctx.headers = rawHeaders
+  })
+
+  app.use(router.routes())
+  app.use(router.allowedMethods())
+
+  const PORT = process.env.PORT || 3000
+
+  app.listen(PORT, () => {
+    console.log(`Running on http://127.0.0.1:${PORT}`)
+  })
+}
+
+init().catch(err => {
+  console.error(err)
+  process.exit(-1)
+})
